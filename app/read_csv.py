e6ab5401413e231879692c7d6784e1a9029c7181
edisonjbernal <edisonjbernal@gmail.com>
2024-01-23 08:51:58 -0500

    read csv files

---
+++ app/read_csv.py
@@ -0,0 +1,23 @@
+import csv
+
+def read_csv(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        headers = next(reader)
+        print(headers)
+        data = []
+        for row in reader:
+            print('---' * 10)
+            iterable = zip(headers, row)
+            #print(list(iterable))
+            country_dict = {key: value for key, value in iterable}
+     
+            data.append(country_dict)
+            #print(row)
+        return data
+
+
+
+if __name__ == '__main__':
+   data = read_csv('./app/world_population.csv')
+   print(data)
